37fd24578576958aa6e2218385cf7d2a89f50280
Hritik Sagar Srivastava <143049203+hritiksagar01@users.noreply.github.com>
2025-07-24 18:06:44 +0530

    [994] [Time Beats: 100.00%] [Memory Beats: 6.09%] - LeetPush

--- Rotting-Oranges.java
+++ Rotting-Oranges.java
@@ -6,11 +6,10 @@ class Solution {
         Queue<int[]> queue = new LinkedList<>();
         int fresh = 0;
 
-        // Step 1: Add all initially rotten oranges to the queue
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
                 if (grid[r][c] == 2) {
-                    queue.offer(new int[]{r, c, 0}); // row, col, minute
+                    queue.offer(new int[]{r, c, 0}); 
                 } else if (grid[r][c] == 1) {
                     fresh++;
                 }
@@ -20,19 +19,17 @@ class Solution {
         int minutes = 0;
         int[][] directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
 
-        // Step 2: BFS
         while (!queue.isEmpty()) {
             int[] current = queue.poll();
             int r = current[0], c = current[1], time = current[2];
-            minutes = time; // track last time level
+            minutes = time; 
 
             for (int[] dir : directions) {
                 int nr = r + dir[0];
                 int nc = c + dir[1];
 
-                // Check if neighbor is a fresh orange
                 if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == 1) {
-                    grid[nr][nc] = 2; // rot it
+                    grid[nr][nc] = 2; 
                     fresh--;
                     queue.offer(new int[]{nr, nc, time + 1});
                 }
