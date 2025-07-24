203238be45d57095b5f64511baa93926054b01a8
Hritik Sagar Srivastava <143049203+hritiksagar01@users.noreply.github.com>
2025-07-24 16:22:48 +0530

    [547] [Time Beats: 87.18%] [Memory Beats: 97.20%] - LeetPush

---
+++ Number-of-Provinces.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+      int[] visited = new int[isConnected.length];
+      int count=0;
+      for(int i =0; i< isConnected.length;i++){
+        if(visited[i] ==0){
+            dfs(isConnected,visited ,i);
+                count++;
+        }
+      }
+      return count;
+    }
+    
+    public void dfs(int[][]M, int[] visited , int i){
+        for( int j =0; j<M.length;j++){
+            if(M[i][j] ==1 && visited[j] ==0){
+                visited[j] =1;
+                dfs(M,visited,j);
+            }
+        }
+    }
+}
