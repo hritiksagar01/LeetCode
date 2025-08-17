e6057931ba035cab0b74e671ba45934bdbe5ad13
Hritik Sagar Srivastava <143049203+hritiksagar01@users.noreply.github.com>
2025-08-18 03:04:53 +0530

    [687] [Time Beats: 96.25%] [Memory Beats: 9.73%] - LeetPush

---
+++ Longest-Univalue-Path.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int longest = 0;
+
+    public int longestUnivaluePath(TreeNode root) {
+        dfs(root);
+        return longest;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) return 0;
+
+        int left = dfs(node.left);
+        int right = dfs(node.right);
+
+        int leftPath = 0, rightPath = 0;
+
+        if (node.left != null && node.left.val == node.val) {
+            leftPath = left + 1;
+        }
+        if (node.right != null && node.right.val == node.val) {
+            rightPath = right + 1;
+        }
+
+        longest = Math.max(longest, leftPath + rightPath);
+        return Math.max(leftPath, rightPath);
+    }
+}
