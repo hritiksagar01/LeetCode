f6e0875918ce522b44191425a32ca68b50d6f8ee
Hritik Sagar Srivastava <143049203+hritiksagar01@users.noreply.github.com>
2026-01-15 18:37:48 +0530

    [1749] [Time Beats: 69.87%] [Memory Beats: 35.06%] - LeetPush

--- Maximum-Absolute-Sum-of-Any-Subarray.java
+++ Maximum-Absolute-Sum-of-Any-Subarray.java
@@ -1,11 +1,20 @@
 1class Solution {
 2    public int maxAbsoluteSum(int[] nums) {
-3        int sum = 0, minSum = 0, maxSum = 0;
-4        for (int num : nums) {
-5            sum += num;
-6            if (sum > maxSum) maxSum = sum;
-7            if (sum < minSum) minSum = sum;
-8        }
-9        return Math.abs(maxSum - minSum);
-10    }
-11}
+3        int maxSoFar = 0;
+4        int minSoFar = 0;
+5        int currMax = 0;
+6        int currMin = 0;
+7
+8        for (int n : nums) {
+9            // Logic for Max Positive Sum
+10            currMax = Math.max(0, currMax + n); // If currMax + n < 0, reset to 0
+11            maxSoFar = Math.max(maxSoFar, currMax);
+12
+13            // Logic for Min Negative Sum
+14            currMin = Math.min(0, currMin + n); // If currMin + n > 0, reset to 0
+15            minSoFar = Math.min(minSoFar, currMin);
+16        }
+17        
+18        return Math.max(maxSoFar, Math.abs(minSoFar));
+19    }
+20}
