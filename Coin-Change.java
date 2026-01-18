c3af1e4bfad43cea7a4893b16a2666c147842a5c
Hritik Sagar Srivastava <143049203+hritiksagar01@users.noreply.github.com>
2026-01-18 11:40:28 +0530

    [322] [Time Beats: 99.58%] [Memory Beats: 76.62%] - LeetPush

--- Coin-Change.java
+++ Coin-Change.java
@@ -1,16 +1,17 @@
-class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount +1];
-        int max = amount +1;
-        Arrays.fill(dp , max);
-        dp[0] =0;
-        for( int i = 1; i <= amount ; i++){
-            for( int coin : coins){
-                if( coin <= i){
-                    dp[i] = Math.min(dp[i] , 1+ dp[i-coin]);
-                }
-            }
-        }
-        return dp[amount] > amount ? -1 : dp[amount];
-    }
-}
+1class Solution {
+2    public int coinChange(int[] coins, int amount) {
+3        int[] dp = new int[amount + 1];
+4        Arrays.fill(dp, amount + 1);
+5
+6        dp[0] = 0;
+7
+8        for (int coin : coins) {
+9            for (int x = coin; x <= amount; x++) {
+10                dp[x] = Math.min(dp[x], 1 + dp[x - coin]);
+11            }
+12        }
+13
+14        return dp[amount] > amount ? -1 : dp[amount];
+15    }
+16}
+17
